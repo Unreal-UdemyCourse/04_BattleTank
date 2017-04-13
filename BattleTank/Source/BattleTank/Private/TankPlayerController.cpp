34bddd51256d57142d8429bd8ad27ec777ae3cc0
DESKTOP-4KPVGQD\Diefleder <the_laughing_bandit@hotmail.com>
2017-04-12 21:17:26 -0700

    BT15 Using Virtual and Override   - created a tank.cpp and TankPlayerController.cpp

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankPlayerController.h"
+
+
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	ATank* Tank = GetControlledTank();
+	if(Tank)
+	UE_LOG(LogTemp, Warning, TEXT("Controlled Tank's Name is %s"), *(Tank->GetName()))
+}
