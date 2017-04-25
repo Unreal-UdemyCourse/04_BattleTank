98eada3d19a8152bf539b62d6fa6a467f7be72f0
DESKTOP-4KPVGQD\Diefleder <the_laughing_bandit@hotmail.com>
2017-04-24 20:41:28 -0700

    BT32 ApplyForceAtLocation in Action   - Set up left track and right track throttle.   - made a tank track c++ class and are applying a force based on the throttle input to the tank scene root.

---
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+
+	// Maximum driving force in neutons.
+	UPROPERTY(EditDefaultsOnly)
+		float TrackMaxDrivingForce = 107200.f;
+
+private:
+
+	
+};
